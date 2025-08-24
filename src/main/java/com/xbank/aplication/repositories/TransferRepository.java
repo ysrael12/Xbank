7de1280b11fde49c9eec05026da4845ce6ae509d
HugooPaixao <hs8622436@gmail.com>
2025-08-24 10:17:06 -0300

    Pequena correção em transfer repository

--- src/main/java/com/xbank/aplication/repositories/TransferRepository.java
+++ src/main/java/com/xbank/aplication/repositories/TransferRepository.java
@@ -4,16 +4,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.xbank.aplication.model.Transfer;
 import com.xbank.aplication.model.ContaPF;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface TransferRepository extends JpaRepository<Transfer, Long> {
 
     // busca  as transferencias feitas por uma conta
-    List<Transfer> findBySource(ContaPF source);
+    List<Transfer> findBySourceAccount(ContaPF sourceAccount);
 
     // busca todas as transferencias recebidas por uma conta
-    List<Transfer> findByDestination(ContaPF destination);
+    List<Transfer> findByDestinationAccount(ContaPF destination);
 
     //busca transferencias por valor
-    List<Transfer> findByAmountGreaterThanEqual(java.math.BigDecimal amount);
+    List<Transfer> findByAmountGreaterThanEqual(BigDecimal amount);
 }
